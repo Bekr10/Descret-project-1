960c135fd6ce42ca03c8705be8756219d11171c7
Ahmed Abobakr <113835453+Bekr10@users.noreply.github.com>
2022-12-28 22:49:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first numper");
+            int numper1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter the second numper");
+            int numper2 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine();
+
+            for (int i = numper1; i <= numper2; i++)
+            {
+                byte numpers = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        numpers++;
+                    }
+
+                }
+                if (numpers == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
